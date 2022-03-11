56fe390a00a81bd5e99fe7bc726c2261ec49d693
cyrstem <cyrstem@gmail.com>
2022-03-11 17:28:00 -0500

    fixes breadcrumbs

--- assets/js/mnprx.js
+++ assets/js/mnprx.js
@@ -7,17 +7,18 @@ const nonprofit = document.getElementById("nonprofit")
 const community = document.getElementById("community ")
 const patron = document.getElementById("patron")
 
+const question =document.getElementById("question");
 
 const more = document.getElementById("more");
 const less = document.getElementById("less");
 
 const studio= document.getElementById("studio");
+const userselection = document.getElementById("subtittle")
 
 const indie= document.getElementById("indie");
 
-const employees = document.getElementById('employees')
-
 const selection = document.getElementById("tittle")
+
 
 const customization  = document.getElementById('customization')
 const subs =document.getElementById('subs')
@@ -25,30 +26,32 @@ const subs =document.getElementById('subs')
 const demo = document.getElementById("demo")
 
 const dis = document.getElementById('disclaimer')
-const reset = document.getElementById('reset');
+const reset = document.getElementById('breadcrumbs');
 
 
 comercial.addEventListener("click", function (){
-  //console.log("click comercial")
   comercial.classList.add("hide");
   nonComercial.classList.add("hide");
   more.classList.remove("hide");
   less.classList.remove("hide")
   selection.innerText = 'Chosen license:  Commercial Use'
-  employees.classList.remove('hide')
+  question.innerText = 'How many employees does your company have?'
   reset.classList.remove("hide")
+
 
 })
 
 nonComercial.addEventListener("click",function(){
   console.log("click noncomercial")
-
   comercial.classList.add("hide");
   nonComercial.classList.add("hide");
   individual.classList.remove("hide");
   nonprofit.classList.remove("hide")
+
   selection.innerText = 'Chosen license : Non-Commercial Use'
+  question.innerText = 'Choose what applies to you'
   reset.classList.remove("hide")
+  
 })
 
 nonprofit.addEventListener("click",function(){
@@ -58,6 +61,7 @@ nonprofit.addEventListener("click",function(){
   patron.classList.remove("hide")
   subs.classList.remove('hide')
   demo.classList.remove('hide')
+  question.classList.add('hide')
 })
 
 individual.addEventListener("click",function(){
@@ -68,38 +72,43 @@ individual.addEventListener("click",function(){
   community.classList.remove("hide");
   subs.classList.remove('hide')
   demo.classList.remove('hide')
+  question.classList.add('hide')
 })
 
 more.addEventListener("click",function(){
   console.log("more");
   more.classList.add("hide");
   less.classList.add("hide")
+  question.classList.add('hide')
   studio.classList.remove('hide')
-  employees.innerHTML = 'Your company has more than 10 employees '
+  userselection.innerHTML = 'Your company has more than 10 employees '
+  userselection.classList.remove('hide');
   customization.classList.remove('hide')
   subs.classList.remove('hide')
   demo.classList.remove('hide')
+  
 })
 
 less.addEventListener("click",function(){
   console.log("less");
   more.classList.add("hide");
   less.classList.add("hide")
+  question.classList.add('hide')
   indie.classList.remove('hide')
-  employees.innerHTML = 'Your company has less than 10 employees '
+  userselection.innerHTML = 'Your company has less than 10 employees '
+  userselection.classList.remove('hide');
   customization.classList.remove('hide')
   subs.classList.remove('hide')
   demo.classList.remove('hide')
+  
 })
 
 
 
 reset.addEventListener("click",function(){
-  console.log("reset");
-
   comercial.classList.remove("hide");
   nonComercial.classList.remove("hide");
-
+  userselection.classList.add('hide');
   more.classList.add('hide');
   less.classList.add('hide')
   individual.classList.add("hide");
@@ -109,8 +118,10 @@ reset.addEventListener("click",function(){
   studio.classList.add('hide')
   indie.classList.add('hide')
   selection.innerText = 'Choose your license'
-  employees.classList.add('hide')
-  employees.innerHTML = 'How many employees does your company have?'
+  
+  question.innerText = 'Choose your license'
+
+  question.classList.remove('hide')
   customization.classList.add('hide')
   subs.classList.add('hide')
   reset.classList.add('hide')
