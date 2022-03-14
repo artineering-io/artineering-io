7239ae3e8762f7538db60f8d9b909e2f047edb93
cyrstem <cyrstem@gmail.com>
2022-03-14 17:08:10 -0500

    changes and corrections according to :https://www.notion.so/artineering/Review-14-03-369b20d45a4348e6bd70554d7ec85e77

--- assets/js/mnprx.js
+++ assets/js/mnprx.js
@@ -9,25 +9,40 @@ const patron = document.getElementById("patron")
 
 const question =document.getElementById("question");
 
-const more = document.getElementById("more");
-const less = document.getElementById("less");
+const more = document.getElementById("morethanten");
+const less = document.getElementById("lessthanten");
 
 const studio= document.getElementById("studio");
-const userselection = document.getElementById("subtittle")
+const userselection = document.getElementById("subtitle")
 
 const indie= document.getElementById("indie");
 
-const selection = document.getElementById("tittle")
-
+const selection = document.getElementById("title-breadcrumb")
 
 const customization  = document.getElementById('customization')
-const subs =document.getElementById('subs')
+const subs =document.getElementById('subscription')
 
 const demo = document.getElementById("demo")
 
 const dis = document.getElementById('disclaimer')
 const reset = document.getElementById('breadcrumbs');
 
+let selectionElements = [];
+selectionElements  =[comercial,nonComercial, individual,nonprofit, more ,less]
+//mouse over only for some elements
+for(var i = 0; i<selectionElements.length; i++){
+  selectionElements[i].onmouseenter =(event) =>{
+    event.target.style.cursor = "pointer";
+    event.target.style.opacity = '0.5'
+  }
+}
+//mouse leave only for  some  elements
+for(var i = 0; i<selectionElements.length; i++){
+  selectionElements[i].onmouseleave =(event) =>{
+    event.target.style.cursor = "pointer";  
+    event.target.style.opacity = '1'
+  }
+}
 
 comercial.addEventListener("click", function (){
   comercial.classList.add("hide");
@@ -38,7 +53,6 @@ comercial.addEventListener("click", function (){
   question.innerText = 'How many employees does your company have?'
   reset.classList.remove("hide")
 
-
 })
 
 nonComercial.addEventListener("click",function(){
@@ -47,9 +61,8 @@ nonComercial.addEventListener("click",function(){
   nonComercial.classList.add("hide");
   individual.classList.remove("hide");
   nonprofit.classList.remove("hide")
-
   selection.innerText = 'Chosen license : Non-Commercial Use'
-  question.innerText = 'Choose what applies to you'
+  subs.classList.add('hide')
   reset.classList.remove("hide")
   
 })
@@ -61,17 +74,20 @@ nonprofit.addEventListener("click",function(){
   patron.classList.remove("hide")
   subs.classList.remove('hide')
   demo.classList.remove('hide')
+  userselection.classList.remove('hide');
+  userselection.innerText = 'You are an Non-Profit / Educational Institution'
   question.classList.add('hide')
 })
 
 individual.addEventListener("click",function(){
   console.log("click individual");
-
   individual.classList.add("hide");
   nonprofit.classList.add("hide");
   community.classList.remove("hide");
   subs.classList.remove('hide')
   demo.classList.remove('hide')
+  userselection.classList.remove('hide');
+  userselection.innerText = 'You are an Individual'
   question.classList.add('hide')
 })
 
@@ -127,3 +143,7 @@ reset.addEventListener("click",function(){
   reset.classList.add('hide')
   demo.classList.add('hide')
 })
+
+function mouseOver(){
+
+}
